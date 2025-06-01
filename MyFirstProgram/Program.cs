3194db4e81212667b422562f43b39362b43126f6
Quaad <ajwood1998@hotmail.co.uk>
2025-06-01 17:15:51 +0200

    Refreshed on basic Math class methods.

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,20 +6,28 @@ namespace MyFirstProgram
     {
         private static void Main(string[] args)
         {
-            int friends = 5;
-            int remainder = 0;
+            double x = 3;
+            double y = 9;
+            double z = -3;
+            double a = 3.14;
 
-            //friends = friends + 1; // Incrementing friends using addition assignment operator
-            //friends += 2; // Incrementing friends using compound assignment operator
-            //friends++; // Incrementing friends using postfix increment operator
+            double powerOf = Math.Pow(x, 2); // raises x to the power of 2 (x^2)
+            double squareRoot = Math.Sqrt(y); // returns the square root of a number
+            double absoluteValue = Math.Abs(z); // returns the absolute value of a number
+            double roundedValue = Math.Round(a, 1); // rounds to the closest tenth. The second argument efines the number of decimal places
+            double ceilingValue = Math.Ceiling(a); // rounds up to the nearest whole number
+            double floorValue = Math.Floor(a); // rounds down to the nearest whole number
+            double maxValue = Math.Max(x, y); // returns the maximum of two numbers
+            double minValue = Math.Min(x, y); // returns the minimum of two numbers
 
-            //friends--; // Decrementing friends using postfix decrement operator
-
-            //friends *= 2; // Multiplying friends by 2 using compound assignment operator
-            //friends /= 2; // Dividing friends by 2 using compound assignment operator
-            remainder = friends % 2; // Modulus operation to find remainder when friends is divided by 2
-
-            Console.WriteLine(remainder); // Output the value of remainder
+            Console.WriteLine("The square of " + x + " is: " + powerOf); // Expected output: 9
+            Console.WriteLine("The square root of " + y + " is: " + squareRoot); // Expected output: 3
+            Console.WriteLine("The absolute value of " + z + " is: " + absoluteValue); // Expected output: 3
+            Console.WriteLine("The rounded value of " + a + " is: " + roundedValue); // Expected output: 3.1
+            Console.WriteLine("The ceiling value of " + a + " is: " + ceilingValue); // Expected output: 4
+            Console.WriteLine("The floor value of " + a + " is: " + floorValue); // Expected output: 3
+            Console.WriteLine("The maximum value between " + x + " and " + y + " is: " + maxValue); // Expected output: 9
+            Console.WriteLine("The minimum value between " + x + " and " + y + " is: " + minValue); // Expected output: 3
 
             Console.ReadKey();
         }
