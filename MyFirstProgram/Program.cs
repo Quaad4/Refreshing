9f78d08c7f1678175987638d2a7825c76f03c77f
Quaad <ajwood1998@hotmail.co.uk>
2025-06-01 17:03:00 +0200

    basic arthimetic refresh

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,14 +6,20 @@ namespace MyFirstProgram
     {
         private static void Main(string[] args)
         {
-            Console.Write("Please write your name: ");
-            String name = Console.ReadLine();
+            int friends = 5;
+            int remainder = 0;
 
-            Console.WriteLine("Hello " + name + "! Welcome to my first program.");
+            //friends = friends + 1; // Incrementing friends using addition assignment operator
+            //friends += 2; // Incrementing friends using compound assignment operator
+            //friends++; // Incrementing friends using postfix increment operator
 
-            Console.Write("What is your age? ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You are " + age + " years old!");
+            //friends--; // Decrementing friends using postfix decrement operator
+
+            //friends *= 2; // Multiplying friends by 2 using compound assignment operator
+            //friends /= 2; // Dividing friends by 2 using compound assignment operator
+            remainder = friends % 2; // Modulus operation to find remainder when friends is divided by 2
+
+            Console.WriteLine(remainder); // Output the value of remainder
 
             Console.ReadKey();
         }
