c5c429ddff6bc74342768a803ed801f0ba0fddf6
Quaad <ajwood1998@hotmail.co.uk>
2025-06-03 12:53:44 +0200

    Created my own number guessing game with error handling and the ability for replayability without needing to relaunch the program. Error handling messages give feedback upon error but allow the program to continue running. E.g. typing a string when a number is required will respond with an error messages that the input format is incorrect and allow the user to type again

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,30 +6,75 @@ namespace MyFirstProgram
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the number guessing game! ");
+            bool playing = true;
+            bool validInput = false;
             Random random = new Random();
-            int randomNumber = random.Next(1, 21); // Generates a random number between 1 and 21
-            double num = random.NextDouble(); // Generates a random double between 0.0 and 1.0
-            Console.WriteLine($"The random number generated is: {randomNumber}");
+            int guess = 0;
+            int guessCount = 0;
+            String input;
 
-            //Array and its size must be declared before it can be used
-            int[] numbers = new int[3];
-            //list is a dynamic array that can grow and shrink in size
-            List<int> myNums = new List<int>();
-
-            for(int i = 0; i < 3; i++)
+            while (playing)
             {
-                //Use Add method to add elements to the list
-                //With a standard array, you would use numbers[i] = random.Next(1, 101);
-                myNums.Add(random.Next(1, 101));
-            }
+                int numberToGuess = random.Next(1, 101); // Random number between 1 and 100
 
-            Console.WriteLine("The random numbers generated are: ");
-            //Using a foreach loop to iterate through the list. Use Count property to get the number of elements in the list
-            //For a typical array, you would use numbers.Length to get the number of elements
-            for (int i = 0; i < myNums.Count; i++)
-            {
-                Console.WriteLine(numbers[i]);
+                while (guess != numberToGuess)
+                {
+                    try
+                    {
+                        Console.Write("Please enter a number between 1 and 100: ");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"{ex.Message}.. Invalid input. Please enter a valid number.");
+                        continue; // Skip to the next iteration of the loop
+                    }
+
+                    if (guess > numberToGuess)
+                    {
+                        Console.Write($"{guess} is too high! Try Again. ");
+                    }
+                    else if (guess < numberToGuess)
+                    {
+                        Console.Write($"{guess} is too low! Try Again. ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Congratulations! {guess} is the correct number! It took you {guessCount} guesses!");
+
+                    }
+                    guessCount++;
+                }
+
+                do
+                {
+                    try
+                    {
+                        Console.Write("Do you want to play again? (y/n): ");
+                        input = Console.ReadLine();
+                        input = input.ToUpper();
+                        switch(input)
+                        {
+                            case "Y":
+                                playing = true;
+                                break;
+                            case "N":
+                                playing = false;
+                                break;
+                            default:
+                                continue;
+                        }
+                        validInput = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"{ex.Message}. Something went wrong. Please try again.");
+                        validInput = false;
+                    }
+                } while (!validInput);
             }
+            Console.WriteLine("Thank you for playing! Goodbye!");
             Console.ReadKey();
         }
     }
