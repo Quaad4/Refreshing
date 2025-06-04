873399677057df920761e9d183ba3975a4d33fc7
Quaad <ajwood1998@hotmail.co.uk>
2025-06-04 22:26:13 +0200

    created interfaces alongside regular inheritance to refresh on the differences

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,31 +6,59 @@ namespace MyFirstProgram
     {
         private static void Main(string[] args)
         {
-            Random random = new Random();
-            int randomNumber = random.Next(1, 21); // Generates a random number between 1 and 21
-            double num = random.NextDouble(); // Generates a random double between 0.0 and 1.0
-            Console.WriteLine($"The random number generated is: {randomNumber}");
+            Rabbit rabbit = new Rabbit();
+            Hawk hawk = new Hawk();
+            Fish fish = new Fish();
 
-            //Array and its size must be declared before it can be used
-            int[] numbers = new int[3];
-            //list is a dynamic array that can grow and shrink in size
-            List<int> myNums = new List<int>();
-
-            for(int i = 0; i < 3; i++)
-            {
-                //Use Add method to add elements to the list
-                //With a standard array, you would use numbers[i] = random.Next(1, 101);
-                myNums.Add(random.Next(1, 101));
-            }
-
-            Console.WriteLine("The random numbers generated are: ");
-            //Using a foreach loop to iterate through the list. Use Count property to get the number of elements in the list
-            //For a typical array, you would use numbers.Length to get the number of elements
-            for (int i = 0; i < myNums.Count; i++)
-            {
-                Console.WriteLine(numbers[i]);
-            }
+            rabbit.Flee();
+            hawk.Hunt();
+            fish.Flee();
+            fish.Hunt();
             Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+    interface IPrey
+    {
+        void Flee();
+    }
+    interface IPredator
+    {
+        void Hunt();
+    }
+
+    class Rabbit : IPrey
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The rabbit hops away quickly!");
+        }
+    }
+
+    class Hawk : IPredator
+    {
+        public void Hunt()
+        {
+            Console.WriteLine("The hawk swoops down to catch its prey!");
+        }
+    }
+
+    class Fish : IPrey, IPredator
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The fish swims away swiftly!");
+        }
+        public void Hunt()
+        {
+            Console.WriteLine("The fish waits patiently for its prey.");
         }
     }
 }
