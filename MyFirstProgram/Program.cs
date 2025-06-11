00c21f7fd582b51e65bebb69edbbea47a3eb979f
Quaad <ajwood1998@hotmail.co.uk>
2025-06-11 16:13:35 +0200

    refactored the input into a for loop to reduce code. also changed the how the success array is instantiated through the use of the length of the angles, as these two sizes must always be the same, therefore allowing for a single number to change to increase or decrease the number of angles required whilst simultaneously ensuring the size of the success array is the same. Also added in some logical comparisons to check the result and another piece of error handling to display different messages as feedback to the user based on their input and the outcome of the program

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -7,28 +7,45 @@ namespace MyFirstProgram
         private static void Main(string[] args)
         {
             int[] angles = new int[3];
-            bool[] success = new bool[3];
+            bool[] success = new bool[angles.Length];
+            bool hasFailed = false;
+            int res = 0;
 
-            Console.Write("Enter the first angle: ");
-            success[0] = int.TryParse(Console.ReadLine(), out angles[0]);
-
-            Console.Write("Enter the second angle: ");
-            success[1] = int.TryParse(Console.ReadLine(), out angles[1]);
-
-            Console.Write("Enter the third angle: ");
-            success[2] = int.TryParse(Console.ReadLine(), out angles[2]);
+            for(int i = 0; i < angles.Length; i++)
+            {
+                Console.Write($"Enter angle number {i + 1}: ");
+                success[i] = int.TryParse(Console.ReadLine(), out angles[i]);
+            }
 
             Console.WriteLine("The angles you entered are:");
             for(int i = 0; i < angles.Length; i++)
             {
                 if (success[i])
                 {
+                    res += angles[i];
                     Console.WriteLine($"Angle {i + 1}: {angles[i]} degrees");
                 }
                 else
                 {
+                    hasFailed = true;
                     Console.WriteLine($"Angle {i + 1}: Invalid input");
                 }
+            }
+
+            if(!hasFailed)
+            {
+                if(res == 180)
+                {
+                    Console.WriteLine("The angles form a valid triangle.");
+                }
+                else
+                {
+                    Console.WriteLine("The angles do not form a valid triangle.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please ensure all inputs are valid integers.");
             }
 
             Console.ReadKey();
