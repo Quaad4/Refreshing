f85af1b4aa2ec5514ee34caf612360d24ec5ed0b
Quaad <ajwood1998@hotmail.co.uk>
2025-06-11 16:04:28 +0200

    refreshing on arrays and creating some minor error handling using a bool array to store the bool result from TryParse()

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,16 +6,29 @@ namespace MyFirstProgram
     {
         private static void Main(string[] args)
         {
-            
-            Console.Write("Enter a Number: ");
+            int[] angles = new int[3];
+            bool[] success = new bool[3];
 
-            if (int.TryParse(Console.ReadLine(), out int userInput))
+            Console.Write("Enter the first angle: ");
+            success[0] = int.TryParse(Console.ReadLine(), out angles[0]);
+
+            Console.Write("Enter the second angle: ");
+            success[1] = int.TryParse(Console.ReadLine(), out angles[1]);
+
+            Console.Write("Enter the third angle: ");
+            success[2] = int.TryParse(Console.ReadLine(), out angles[2]);
+
+            Console.WriteLine("The angles you entered are:");
+            for(int i = 0; i < angles.Length; i++)
             {
-                Console.WriteLine($"You entered: {userInput}");
-            }
-            else
-            {
-                Console.WriteLine("You did not enter a number or entered 0, which is not a valid number.");
+                if (success[i])
+                {
+                    Console.WriteLine($"Angle {i + 1}: {angles[i]} degrees");
+                }
+                else
+                {
+                    Console.WriteLine($"Angle {i + 1}: Invalid input");
+                }
             }
 
             Console.ReadKey();
