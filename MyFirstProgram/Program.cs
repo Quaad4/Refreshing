5975811c9bcdf4acecbca45989b3f60b01ea7dd0
Quaad <ajwood1998@hotmail.co.uk>
2025-06-12 21:40:35 +0200

    practising with lists

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,39 +6,22 @@ namespace MyFirstProgram
     {
         private static void Main(string[] args)
         {
-            int[] angles = new int[3];
-            bool[] success = new bool[angles.Length];
-            bool hasFailed = false;
-            int res = 0;
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
 
-            for(int i = 0; i < angles.Length; i++)
-            {
-                Console.Write($"Enter angle number {i + 1}: ");
-                success[i] = int.TryParse(Console.ReadLine(), out angles[i]);
-            }
+            int index = numbers.IndexOf(3);
 
-            Console.WriteLine("The angles you entered are:");
-            for(int i = 0; i < angles.Length; i++)
+            if(index >= 0)
             {
-                if (success[i])
+                numbers.RemoveAt(index);
+                Console.WriteLine($"Found at index {index}");
+                foreach (int number in numbers)
                 {
-                    res += angles[i];
-                    Console.WriteLine($"Angle {i + 1}: {angles[i]} degrees");
+                    Console.WriteLine(number);
                 }
-                else
-                {
-                    hasFailed = true;
-                    Console.WriteLine($"Angle {i + 1}: Invalid input");
-                }
-            }
-
-            if(!hasFailed)
-            {
-                Console.WriteLine(res.Equals(180) ? "The angles form a valid triangle." : "The angles do not form a valid triangle.");
             }
             else
             {
-                Console.WriteLine("Please ensure all inputs are valid integers.");
+                Console.WriteLine("not found in the list.");
             }
 
             Console.ReadKey();
