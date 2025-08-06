abb6e754f690f449e60492e55f863b353f06e46d
Quaad <ajwood1998@hotmail.co.uk>
2025-08-06 20:47:42 +0200

    added new classes and an interface to introduce simple methods to borrow and return a library item. Next steps will introduce a Library class which will store the collection of Library items and allow for listing all items, listing available items, adding, and finding by title (exact match to begin with)

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -8,28 +8,37 @@ namespace MyFirstProgram
         {
             Book book = new Book("Clean Code", "Robert C. Martin");
             book.DisplayInfo();
+
+            Magazine magazine = new Magazine("The Lord of the Rings", 123);
+            magazine.DisplayInfo();
+
+            DVD dvd = new DVD("The Matrix", 160);
+            dvd.DisplayInfo();
+
             Console.ReadKey();
         }
     }
 
     abstract class LibraryItem
     {
-        public static int _NextId;
+        private static int NextId = 1;
         public int Id { get; }
         public string Title { get; set; }
 
         public LibraryItem(string title)
         {
-            Id = _NextId++;
+            Id = NextId++;
             Title = title;
         }
 
         public abstract void DisplayInfo();
     }
 
-    class Book : LibraryItem
+    class Book : LibraryItem, IBorrowable
     {
         public string Author { get; set; }
+        public string BorrowerName { get; set; }
+        public bool IsBorrowed { get; private set;  }
 
         public Book(string title, string author) : base(title)
         {
@@ -38,8 +47,104 @@ namespace MyFirstProgram
 
         public override void DisplayInfo()
         {
-            Console.WriteLine($"Book [id: {Id}] - \'{Title}\' by {Author}");
+            Console.WriteLine($"Book [Id: {Id}] - \"{Title}\" by {Author}");
         }
 
+        public void Borrow(string borrowerName)
+        {
+            if (!IsBorrowed)
+            {
+                BorrowerName = borrowerName;
+                IsBorrowed = true;
+                Console.WriteLine($"{Title} has been borrowed by {BorrowerName}.");
+            }
+            else
+            {
+                Console.WriteLine($"{Title} is already borrowed.");
+            }
+        }
+
+        public void Return()
+        {
+            if (IsBorrowed)
+            {
+                Console.WriteLine($"{Title} has been returned by {BorrowerName}.");
+                IsBorrowed = false;
+                BorrowerName = null;
+            }
+            else
+            {
+                Console.WriteLine($"{Title} is not currently borrowed.");
+            }
+        }
+
+    }
+
+    class Magazine : LibraryItem
+    {
+        public int IssueNumber { get; set; }
+
+        public Magazine(string title, int issueNumber) : base(title)
+        {
+            IssueNumber = issueNumber;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Magazine [Id: {Id}] - \"{Title}\" Issue #{IssueNumber}");
+        }
+    }
+
+    class DVD : LibraryItem, IBorrowable
+    {
+        public int DurationInMinutes { get; set; }
+        public string BorrowerName { get; set; }
+        public bool IsBorrowed { get; private set; }
+
+        public DVD(string title, int durationInMinutes) : base(title)
+        {
+            DurationInMinutes = durationInMinutes;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"DVD [Id: {Id}] - \"{Title}\" - Duration: {DurationInMinutes} minutes");
+        }
+
+        public void Borrow(string borrowerName)
+        {
+            if (!IsBorrowed)
+            {
+                BorrowerName = borrowerName;
+                IsBorrowed = true;
+                Console.WriteLine($"{Title} has been borrowed by {BorrowerName}.");
+            }
+            else
+            {
+                Console.WriteLine($"{Title} is already borrowed.");
+            }
+        }
+
+        public void Return()
+        {
+            if (IsBorrowed)
+            {
+                Console.WriteLine($"{Title} has been returned by {BorrowerName}.");
+                IsBorrowed = false;
+                BorrowerName = null;
+            }
+            else
+            {
+                Console.WriteLine($"{Title} is not currently borrowed.");
+            }
+        }
+    }
+
+    interface IBorrowable
+    {
+        bool IsBorrowed { get; }
+
+        void Borrow(string borrowerName);
+        void Return();
     }
 }
