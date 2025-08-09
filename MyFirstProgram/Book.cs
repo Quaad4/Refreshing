e482633b1b5f808bef75fa6bebe1bc807c21329e
Quaad <ajwood1998@hotmail.co.uk>
2025-08-09 14:57:29 +0200

    Separated classes and the interface into separate files to make code more readable and easier to maintain

---
+++ MyFirstProgram/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Book : BorrowableItem
+    {
+        public string Author { get; set; }
+
+        public Book(string title, string author) : base(title)
+        {
+            Author = author;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Book [Id: {Id}] - \"{Title}\" by {Author}");
+        }
+    }
+}
---
+++ MyFirstProgram/Magazine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Magazine : LibraryItem
+    {
+        public int IssueNumber { get; set; }
+
+        public Magazine(string title, int issueNumber) : base(title)
+        {
+            IssueNumber = issueNumber;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Magazine [Id: {Id}] - \"{Title}\" Issue #{IssueNumber}");
+        }
+    }
+}
